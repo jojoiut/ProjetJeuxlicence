4cd730456ac0c95fb0cf9c7000b36b21f2f76f3a
Christelle92 <32565520+Christelle92@users.noreply.github.com>
2017-10-10 10:44:33 +0200

    Add files via upload

---
+++ squeletteUML/IA.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IA : Joueur {
+
+	public IA(string name,Specialisation spe,Spawn spawn): base(name,spe,spawn){
+			
+	}
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
